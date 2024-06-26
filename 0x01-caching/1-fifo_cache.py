7bc1914a4411c7fb5e0e8f206e4b1d53063203b2
Fati-Zid <ezzaidanif@gmail.com>
2024-06-27 00:21:08 +0100

    all

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
-""" FIFO Cache module """
+''' FIFO Cache module '''
+
 
 from base_caching import BaseCaching
 
+
 class FIFOCache(BaseCaching):
-    """ FIFOCache is a caching system that inherits from BaseCaching
+    ''' FIFOCache is a caching system that inherits from BaseCaching
         and uses FIFO eviction policy.
-    """
+    '''
 
     def __init__(self):
-        """ Initialize """
+        ''' Initialize '''
         super().__init__()
         self.queue = []
 
     def put(self, key, item):
-        """ Add an item in the cache """
+        ''' Add an item in the cache '''
         if key is not None and item is not None:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
                 first_key = self.queue.pop(0)
@@ -25,5 +27,5 @@ class FIFOCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key """
+        ''' Get an item by key '''
         return self.cache_data.get(key, None)
